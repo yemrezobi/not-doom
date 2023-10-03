54fd505f4514d19ba86d439edf58c5cbd9981c11
Yunus Emre Zobi <y.emrezobi@gmail.com>
2023-10-03 20:38:23 +0300

    Remove unnecessary scripts

--- build/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -eu
-
-cmake .. -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE
-cppcheck ../src --project=compile_commands.json --std=c++20 --language=c++ --suppress='*:_deps/*' -j4 -v -q
-cmake --build .
-ctest
--- build/clean.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -r CMakeCache.txt CMakeFiles/ Makefile cmake_install.cmake compile_commands.json release/
